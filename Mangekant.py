b8c9ba2b4a538a05b332d0198cc42f219ae0c177
Ismail Ali <ismailali2032@gmail.com>
2024-11-03 22:52:30 +0100

    aunivers oppgave ferdigstilt

--- Mangekant.py
+++ Mangekant.py
@@ -20,3 +20,46 @@ liste.append(kvadrat)
 
 for ting in liste:
     print(ting.visinfo())
+
+class Rektangel(Mangekant):
+    def __init__(self, navn, lengde, bredde):
+        super.__init__(navn)
+        self.lengde = lengde
+        self.bredde = bredde
+
+    def arealrektangel(self):
+        return self.lengde * self.bredde
+    
+
+class Kvadrat(Rektangel):
+    def __init__(self, navn, lengde, ):
+        super().__init__(navn, lengde,)
+    def arealkvadrat(self):
+        return self.lengde * self.lengde
+
+import math as m
+
+class Trekant(Mangekant):
+    def __init__(self, navn: str, antall_sider: int, sidelengder: list):
+        super().__init__(navn, antall_sider, sidelengder)
+    def arealtrekant(self):
+        s = self.omkrets() * 0.5  
+        return round(m.sqrt(s * (s - self.sidelengder[0]) * (s - self.sidelengder[1]) * (s - self.sidelengder[2])),1)
+    def vistrekant(self):
+        return f'{self.navn}: Sidelengder: {self.sidelengder}, Areal:{self.arealtrekant()}'
+
+trekant_liste = []
+trekant1 = Trekant("Trekant 1",3,[4,4.2,5])
+trekant2 = Trekant("Trekant 2",3,[5,5.6,5.6])
+trekant3 = Trekant("Trekant 3",3,[4,4,4])
+trekant4 = Trekant("Trekant 4",3,[4,6,7])
+trekant5 = Trekant("Trekant 5",3,[3,6.2,6.2])
+
+trekant_liste.append(trekant1)
+trekant_liste.append(trekant2)
+trekant_liste.append(trekant3)
+trekant_liste.append(trekant4)
+trekant_liste.append(trekant5)
+
+for ting in trekant_liste:
+    print(ting.vistrekant())
