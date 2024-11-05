d26af28a50ac3637b44c6b039f147e8809fed423
jonaball <jonathan.tonseth@gmail.com>
2024-11-05 09:49:06 +0100

    bruke sidekanter for trekant areal

--- ledetekst_frontend.py
+++ ledetekst_frontend.py
@@ -9,33 +9,38 @@ spørsmål1 = inq.List('figur',
             message="Hvilken type mangekant velger du?",
             choices=['Trekant', 'Rektangel', 'Kvadrat'],
         ), 
+mangekant_select = "" #holder track over hvilken type mangekant som er valgt
 
 spørsmål2 = inq.List('operasjon',
-             message="Hva slags operasjon vil du gjøre med mangekanten?",
-             choices=['Regne Areal', 'Regne Omkrets', "Lagre en Mangekant"],
+            message="Hva slags operasjon vil du gjøre med mangekanten?",
+            choices=['Regne Areal', 'Regne Omkrets', "Lagre en Mangekant"],
         ),
 
-mangekant_select = "" #holder track over hvilken type mangekant som er valgt
+spørsmål3 = inq.List('trekant', 
+            message="Har du alle sidelengdene til trekanten?",
+            choices=['Ja', 'Nei']
+        ),
 
 # ------------ Svar-alternativer ----------- #
 
 svar1 = inq.prompt(spørsmål1)
 if svar1['figur'] == 'Trekant':
     mangekant_select = "trekant"
-
 if svar1['figur'] == 'Rektangel':
     mangekant_select = "rektangel"
-
 if svar1['figur'] == 'Kvadrat':
     mangekant_select = "kvadrat"
 
 svar2 = inq.prompt(spørsmål2)
-
 if svar2['operasjon'] == "Regne Areal":
     if mangekant_select == "trekant":
-        grunnlinje = int(input("Grunnlinje lengde: "))
-        høyde = int(input("Høyde lengde: "))
-        print(f.arealTrekant(grunnlinje, høyde))
+        svar3 = inq.prompt(spørsmål3)
+        if svar3['operasjon'] == 'Ja':
+            m.Trekant.arealtrekant
+        else:
+            grunnlinje = int(input("Grunnlinje lengde: "))
+            høyde = int(input("Høyde lengde: "))
+            print(f.arealTrekant(grunnlinje, høyde))
     if mangekant_select == "rektangel":
         grunnlinje = int(input("Bredde: "))
         høyde = int(input("Høyde: "))
@@ -50,9 +55,7 @@ if svar2['operasjon'] == "Regne Omkrets":
 if svar2['operasjon'] == "Lagre en Mangekant":
     if mangekant_select =="trekant":
         m.trek_define()
-
     if mangekant_select =="rektangel":
         m.rekt_define()
-
     if mangekant_select == "kvadrat":
         m.kvad_define()
